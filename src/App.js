032427441b03aeaa7fdd802b920f22cf33226479
Ferit özcan <feritzcan93@gmail.com>
2022-12-14 00:09:02 +0300

    affassda

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import WelcomePanel from './Components/WelcomePanel';
 import images from './Utils/ImageLoader';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import image1 from './Images/A - ALACAKARANLIK KUŞLARI - 1. SAYFA.png';
-import image2 from './Images/A - ALACAKARANLIK KUŞLARI - 1. SAYFA.png';
-import image3 from './Images/A - ALACAKARANLIK KUŞLARI - 1. SAYFA.png';
-import image4 from './Images/A - ALACAKARANLIK KUŞLARI - 1. SAYFA.png';
+
 
 
 
@@ -22,7 +19,7 @@ function App() {
       {DropDownPanel("ferit")}
 
       <div className="image-slider">
-        <img src={images['A - ALACAKARANLIK KUŞLARI - 1. SAYFA.png']} />
+        <img src={images['A - ALACAKARANLIK KUŞLARI - 1. SAYFA.jpeg']} />
         {/* <img src={image2} />
         <img src={image3} />
         <img src={image4} /> */}
